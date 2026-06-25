e2d4f7744213d5797c86ed93b280d4771c20903f
Madhu <madhudugyala9963@gmail.com>
2026-06-25 09:43:24 +0530

    Add files via upload

---
+++ tax.py
@@ -0,0 +1,18 @@
+t=int(input("enter salary:"))
+if t>0:
+    if t<=250000:
+        print("No tax")
+    elif t<=500000:
+        t=t-250000
+        t=(t*5)/100
+        print("5% tax",t)
+    elif t<1000000:
+        t=t-250000
+        t=(t*7)/100
+        print("7% ",t)
+    elif t>=1000000:
+        t=t-250000
+        t=(t*10)/100
+        print("10%",t)
+else:
+    print("enter a valid emount")
